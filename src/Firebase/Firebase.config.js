9d8fac5dc9c64722e69963117a77493a7259346b
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2023-12-24 20:56:44 +0600

    creact contact

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,15 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey:import.meta.env.VITE_apiKey,
-    authDomain:import.meta.env.VITE_authDomain,
-    projectId:import.meta.env.VITE_projectId,
-    storageBucket:import.meta.env.VITE_storageBucket,
-    messagingSenderId:import.meta.env.VITE_messagingSenderId,
-    appId:import.meta.env.VITE_appId
+  apiKey: "AIzaSyCcz8Uy4eLsIqljIbOqf5coe9QxLVRMYCk",
+  authDomain: "all-in-one-653c0.firebaseapp.com",
+  projectId: "all-in-one-653c0",
+  storageBucket: "all-in-one-653c0.appspot.com",
+  messagingSenderId: "742105878895",
+  appId: "1:742105878895:web:18cfb0b643f9dfa83f989a"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app ;
